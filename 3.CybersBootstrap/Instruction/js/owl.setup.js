42733c6a45f8172ced0d9fa5c7787b31499bfdb4
VietAnhVu <vva1994@gmail.com>
2021-08-22 03:14:43 +0700

    :)

---
+++ 3.CybersBootstrap/Instruction/js/owl.setup.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false, //xóa mũi tên
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        }
+    }
+})
