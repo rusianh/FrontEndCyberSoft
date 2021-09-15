c630886425bfc8b195ba0539ccad787344daf061
VietAnhVu <vva1994@gmail.com>
2021-09-16 03:18:34 +0700

    :)

--- 6.JavascriptES6/Demo/js/demo.js
+++ 6.JavascriptES6/Demo/js/demo.js
@@ -125,11 +125,213 @@ let hocVien = {
 /**
  * !Default Parameter Values
 */
-
+//*ES5
 function getUserInfo(name, age){
+    //C1: Dùng if else
+    // if(name == undefined || age == undefined){
+    //     console.log("Gia trị không hợp lệ");
+    //     name = "default name";
+    //     age = 18;
+    // }
+
+    //C2: ternary operator
+    // name = name == undefined ? "default name" : name;
+    // age = age == undefined ? 31 :  name;
+    
+    //C3: Toán tử ||
+    //underfine, null, NaN
+    // name = name || "default name";
+    // age = age || "48";
+
+    // if(age >= 18 && age <30  ){
+    //     console.log(name + " đang còn trẻ " + name + " nên đi chơi nhiều");
+    // } else {
+    //     console.log(name + " đã "+ age + " tuổi. Nên ở nhà") ;
+    // }
+}
+
+// getUserInfo();
+
+//*ES6 ****
+//Đặt giá trị defaul ngay lúc khai báo, sử dụng arrow function
+let getUserInfoES6 = (name = "Mị", age = "48")=>{
     if(age >= 18 && age <30  ){
-        console.log(name + " đang còn trẻ" + name + " nên đi chơi nhiều");
+        console.log(name + " đang còn trẻ " + name + " nên đi chơi nhiều");
     } else {
         console.log(name + " đã "+ age + " tuổi. Nên ở nhà") ;
     }
 }
+
+// getUserInfoES6();
+
+/**
+ * !Rest Parameter
+ * Dùng để khai báo hàm với số lượng tham số không xác định
+ * Các tham số truyền vào thành 1 mảng
+ * Để khai báo => đặt 3 dấu chấm trước biến đại diện
+ **/
+
+let tinhDiemTrungBinh = (...listDiem)=> {
+    console.log(listDiem);
+    let tongDiem = 0;
+    for(let i = 0; i< listDiem.length; i++){
+        tongDiem += listDiem[i];
+    }
+    console.log("tong diem TB la: ",tongDiem/listDiem.length);
+}
+// tinhDiemTrungBinh(8,9,10,6);
+
+/**
+ * !Spread Operator
+ * Cú pháp giống Rest Parameter
+ * Khác với rest nó nhận vào mảng và trả ra từng phần tử
+ * Dùng thêm phần tử vào mảng hoặc thêm thuộc tính vào obj
+ **/
+
+//Them phần tử vào mảng
+let mangC = [1,2,3,4];
+let mangD = [...mangC];
+mangD.push(5);
+
+//Thêm thuộc tính cho đối tượng, có thể dung prototype để them đối tượng hoặc spread operator
+let pet = {
+    ten: "be na",
+    bread: "snake"
+}
+
+let newPet = {
+    age: "2",
+    ...pet
+}
+
+// console.log(newPet);
+
+// console.log(mangD)
+
+/**
+ * !Destructuring
+ * Là một kỹ thuật dùng để lấy một hay nhiều giá trị của phần tử của mảng hoặc thuộc tính của obj, sau đó gán các giá trị này cho các biến khai báo trước
+ * Cú pháp ngắn gọn hơn, tránh lặp code
+ **/
+
+//*Array
+let program = ["javascript", "java", "python"];
+
+//ES5
+// console.log(program[0]);
+
+//ES6: Cú pháp Destructuring
+// let [first, second, third] = program;
+// console.log(second);
+
+//*Object
+let pet1 = {
+    name1: "Gâu đần",
+    age: 3,
+    breed: "golden",
+    size: {
+        weight: "30kg",
+        height: "56cm"
+    }
+}
+
+//ES5
+// var name = pet1.name;
+// var age = pet1.age;
+// var size = pet1.size;
+
+// console.log(size);
+
+//ES6
+let {name1,age} = pet1;
+let{weight, height} = pet1.size;
+// console.log(name1);
+// console.log(weight);
+
+let{weight:w, height:h} = pet1.size;
+// console.log(w,h);
+
+/**
+ * !Template String
+ * Template String còn gọi là template literals là cú pháp khai báo String trong ES6
+ * Cho phép sử dụng multiline string, biến, biểu thức, hàm bên trong string mà không phải thông qua phép cộng String.
+ **/
+
+let fish = "cá";
+let action = "bơi";
+
+//*Mình là cá, việc của mình là bơi
+//*ES5
+// console.log("Mình là " + fish + " Việc của mình là " + action);
+//*ES6
+// console.log(`Mình là ${fish} việc của mình là ${action}`);
+
+let a = 1;
+let b = 2;
+// console.log(``)
+// document.getElementById("section1").innerHTML = `
+// <div class="class1">
+//     <p>Mình là ${fish} việc của mình là ${action}</p>
+// </div>
+// `;
+
+/**
+ * !Object Literal
+ * 
+ **/
+
+//*ES5
+// var namee = "Mị;"
+// var myObject = {
+//     namee: namee,
+//     sayHi: function(){
+//         console.log("Hi my name is "+ namee);
+//     }
+// }
+
+// myObject.sayHi();
+
+//*ES6
+// let namee = "Mị;"
+// let myObject = {
+//     namee,
+//     sayHi(){
+//         console.log("Hi my name is "+ namee);
+//     }
+// }
+
+// myObject.sayHi();
+
+//*tên động
+// let namee = "ten";
+// let myObject = {
+//     //ten: "Mị"
+//     [namee]: "Mị",
+//     sayHi(){
+//         console.log("Hi my name is "+ this.ten);
+//     }
+// }
+
+// myObject.sayHi();
+
+//*for
+let curencies = ["vnd",, "usd", "SGN"];
+// for(let i =0; i<curencies.length ; i++){
+//     console.log(curencies[i]);
+// }
+
+//ES5
+//*for in
+// for(let i in curencies){
+//     console.log(curencies[i]);
+// }
+
+//ES6
+//*for of
+// for(let value of curencies){
+//     console.log(value);
+// }
+
+// for(let [index, value] of curencies.entries()){
+//     console.log(index, value);
+// }
---
+++ 6.JavascriptES6/Demo/js/oopES6.js
@@ -0,0 +1,104 @@
+
+//*ES6
+// class Student {
+//     constructor(name, address){
+//         this.name = name;
+//         this.address = address;
+//     }
+// }
+
+// let student = new Student("Mị", "HaNoi");
+// console.log(student.name, student.address);
+
+//!Extend - Tính kế thừa
+//*ES5
+function Father(name){
+    this.name = name;
+}
+Father.prototype.creatColorEyes = function(){
+    console.log("black");
+}
+
+function Children(name){
+    //Kế thừa thuộc tính từ Father
+    Father.apply(this,arguments);
+}
+//Kế thừa phước thức từ Father 
+Children.prototype = new Father;
+//Override
+Children.prototype.creatColorEyes = function(){
+    console.log("white");
+}
+//Tạo thuộc tính riêng cho con
+Children.prototype.createColorSkin = function(){
+    console.log("Pink");
+}
+
+// var child = new Children("hi");
+// console.log(child.name);
+// child.creatColorEyes();
+
+//*ES6
+class Person {
+    constructor(name){
+        this.name = name;
+    }
+    createColorEyes(){
+        console.log("black");
+    }
+}
+
+//Extend
+class Manager extends Person {
+    //super
+    age = 19;
+    getEeys(){
+        super.createColorEyes;
+    }
+
+    createColorSkin(){
+        console.log("ColorSkin");
+    }
+}
+
+let manager = new Manager("Manger");
+// console.log(manager.name);
+// manager.createColorSkin();
+// console.log(manager.age);
+
+//!Proxy | Get giúp lấy giá trị ra và xử lý, rồi xuất ra bên ngoài và 
+//set - giúp set lại gia trị thuộc tính đang muốn truy cập, và set chỉ có kiểu trả về boolean => set dc trả về true và ngc lại.
+
+let pet2 = {
+    name : "cau vang",
+    age : 2,
+    breed : "shiba"
+}
+
+// let pet3 = new Proxy(pet2,{
+//     get(target,prop, receiver){
+//         if(typeof target[prop] ==='string'){
+//             return target[prop].toUpperCase();
+//         }
+
+//         return target[prop];
+//     }
+// })
+// console.log(pet2.name);
+// console.log(pet3.age);
+// console.log(pet3.name);
+
+
+let pet3 = new Proxy(pet2,{
+    set(target,prop, val){
+        if(prop === "age" && typeof val !== 'number'){
+            throw new TypeError("Age must be number");
+        }
+
+        target[prop]= val;
+        return true;
+    }
+})
+
+// pet3.age = "123";
+pet3.age = 10;
