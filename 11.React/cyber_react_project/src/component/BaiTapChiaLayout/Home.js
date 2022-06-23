85886eb013423176156709dc215b587e0e78cdf8
Cris Dev <vva1994@gmail.com>
2022-06-23 21:04:51 +0700

    🌈~

--- 11.React/cyber_react_project/src/component/BaiTapChiaLayout/Home.js
+++ 11.React/cyber_react_project/src/component/BaiTapChiaLayout/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import Content from './Content'
+import HeaderDemo from './HeaderDemo'
+import Navigation from './Navigation'
 
 export default class Home extends Component {
   render() {
@@ -6,7 +9,15 @@ export default class Home extends Component {
       <div className='container-fluid '>
            <div className="row">
                 <div className="col-md-12">
-                    Header
+                    <HeaderDemo/>
+                </div>
+           </div>
+           <div className="row">
+                <div className="col-md-4">
+                    <Navigation/>
+                </div>
+                <div className="col-md-8">
+                    <Content/>
                 </div>
            </div>
       </div>
